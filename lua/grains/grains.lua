cad7e40c42a51e8b7d16448b74afbadeff422f77
denismaier <denis.maier@ub.unibe.ch>
2022-02-01 14:35:15 +0100

    first try

--- lua/grains/grains.lua
+++ lua/grains/grains.lua
@@ -1,11 +1,30 @@
 local grains = {}
 
 function grains.square(n)
-
+    result = 2^(n-1)
+    return result
 end
 
+-- approach with a loop
 function grains.total()
+    result = 0
+    for i = 1,64,1
+    do
+        result = result + grains.square(i)
+    end
+    return result
+end
 
+-- recursive approach; won't work
+function grains.totalTill(n)
+    next = n-1
+    result = grains.square(n) + grains.totalTill(next)
+    return result
+end
+
+function grains.totalRecursive()
+    result = grains.totalTill(64)
+    return result
 end
 
 return grains
