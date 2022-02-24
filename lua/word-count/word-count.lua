e0d4515d8b2e3784d29d9554b492c4629cdc70a3
denismaier <denis.maier@ub.unibe.ch>
2022-02-24 10:06:40 +0100

    simplify

--- lua/word-count/word-count.lua
+++ lua/word-count/word-count.lua
@@ -2,23 +2,15 @@ local function word_count(s)
   -- normalize
   s = s:lower()
   -- normalize whitespace
-  s = string.gsub(s, "%s", " ")
+  s = s:gsub("%s", " ")
   -- remove trailing single quotes
-  -- s = string.gsub(s, "%A'%A", "") -- does not work, why?
-  -- so remove trailing single quotes one be
-  s = string.gsub(s, "%s'", " ")
-  s = string.gsub(s, "'%s", " ")
-  s = string.gsub(s, "^'", "")
-  s = string.gsub(s, "'$", "")
+  s = s:gsub("%s'", " "):gsub("^'", "")
+  s = s:gsub("'%s", " "):gsub("'$", "")
   --
   results = {}
-  pattern = "[%a%d']+"
+  pattern = "[%w']+"
   for word in string.gmatch(s, pattern) do
-    if results[word] == nil then 
-      results[word] = 1 
-    else
-      results[word] = results[word] + 1
-    end
+    results[word] = (results[word] or 0) + 1
   end
   return results
 end
