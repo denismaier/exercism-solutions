a5484e05fd494ce08cdb9c0f27a6bfdae7a2a981
denismaier <denis.maier@ub.unibe.ch>
2022-02-25 10:06:07 +0100

    simplifications

--- lua/anagram/anagram.lua
+++ lua/anagram/anagram.lua
@@ -3,44 +3,29 @@ Anagram.__index = Anagram
 
 function Anagram:new(str)
     local self = setmetatable({}, Anagram)
-	self.content = str
+	self.original = str
+    self.sorted = sortString(str)
 	return self
 end
 
 function Anagram:match(strs)
     local result = {}
-    for _, str in ipairs(strs) do 
-        if is_anagram(self.content, str) then table.insert(result, str) end
+    for _, str in ipairs(strs) do
+        strToMatch = sortString(str)
+        if (str ~= self.original and strToMatch == self.sorted) then table.insert(result, str) end
     end
     return result
 end
 
+-- helper
 
--- helper functions
-
-function is_anagram(str1, str2)
-    -- string length not equal
-    if str1:len() ~= str2:len() then return false end
-    return arrayEqual(string_to_sorted_table(str1), string_to_sorted_table(str2))
-end
-
-function string_to_sorted_table (str)
+function sortString (str)
+    local letters = {}
     str = str:lower()
-    local result = {}
-    for i = 1, string.len(str), 1 do
-        table.insert(result, str:sub(i, i))
-    end
-    table.sort(result)
-    return result
-end
-
-function arrayEqual(a1, a2)
-    -- arrays length not equal (not strictly necesary since we already know this)
-    if #a1 ~= #a2 then return false end
-    for i, v in ipairs(a1) do
-        if v ~= a2[i] then return false end
-    end
-    return true
+    for char in string.gmatch(str, ".") do table.insert(letters, char) end
+    table.sort(letters)
+    local sortedStr = table.concat(letters)
+    return sortedStr
 end
 
 return Anagram
