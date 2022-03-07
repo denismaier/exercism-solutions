202bba9cc14058f48bf914fb561dedd3edf9469b
denismaier <denis.maier@ub.unibe.ch>
2022-03-07 10:44:04 +0100

    improvements

--- lua/phone-number/phone-number.lua
+++ lua/phone-number/phone-number.lua
@@ -13,14 +13,14 @@ function PhoneNumber:areaCode()
     return string.sub(self.number, 1, 3)
 end
 
+function PhoneNumber:__tostring()
+    return self.number:gsub("(...)(...)(....)", "(%1) %2-%3")
+end
+
 function validate_number(input)
     if string.len(input) == 10 then return input end
     if (string.len(input) == 11 and string.sub(input, 1, 1) == '1') then return string.sub(input, 2) end
     return '0000000000'
-end
-
-function PhoneNumber:__tostring()
-    return self.number:gsub("(...)(...)(....)", "(%1) %2-%3")
 end
 
 return PhoneNumber
