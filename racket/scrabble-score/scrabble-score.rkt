420139e65b7752ac6b90dd8a1e8c0b05a8271be7
denismaier <maier.de@gmail.com>
2022-03-23 09:33:42 +0100

    fixes

--- racket/scrabble-score/scrabble-score.rkt
+++ racket/scrabble-score/scrabble-score.rkt
@@ -89,16 +89,16 @@
   '(((#\a #\e #\i #\o #\u #\l #\n #\r #\s #\t) 1)
     ((#\d #\g) 2)
     ((#\b #\c #\m #\p) 3)
-    ((#\f #\h"#\v"#\w #\y) 4)
+    ((#\f #\h #\v #\w #\y) 4)
     ((#\k) 5)
     ((#\j #\x) 8)
     ((#\q #\z) 10)
     ))
 
 (define (alist->hash alist)
-  (for*/hash ([category alist]
-              [key (first category)])
-    (values key (second category))))
+  (for*/hash ([assoc alist]
+              [key (first assoc)])
+    (values key (second assoc))))
 
 (define (score-via-for-from-hash word)
   (define word-normalized (string-downcase word))
