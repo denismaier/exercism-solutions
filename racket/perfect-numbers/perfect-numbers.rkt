277dedf424e3c8d4798d449f1f1e196be6726172
denismaier <maier.de@gmail.com>
2022-06-05 11:50:09 +0200

    improvements

--- racket/perfect-numbers/perfect-numbers.rkt
+++ racket/perfect-numbers/perfect-numbers.rkt
@@ -2,12 +2,25 @@
 
 (provide classify)
 
-(define (aliquot-sum n)
-  (for/sum ([i
-             (for/list ([i (range 1 (- n 1))]
+(define (list-of-factors n)
+  (for/list ([i (inclusive-range 1 (quotient n 2))]
              #:when (= 0 (modulo n i)))
-               i)])
+               i))
+
+(define (aliquot-sum-via-for n)
+  (for/sum ([i (list-of-factors n)])
     i))
+
+(define (aliquot-sum-via-apply n)
+  (apply + (list-of-factors n)))
+
+(define (aliquot-sum-via-foldl n)
+  (foldl + 0 (list-of-factors n)))
+
+(define aliquot-sum
+  aliquot-sum-via-foldl)
+  ;aliquot-sum-via-for)
+  ;aliquot-sum-via-apply)
 
 (define (classify n)
   (define perfect (aliquot-sum n))
