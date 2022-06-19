a285eb852e1e30ba7be81b0e33c2193bbdf2f018
denismaier <denis.maier@ub.unibe.ch>
2022-06-19 11:25:01 +0200

    initial commit bob.lua

--- lua/bob/bob.lua
+++ lua/bob/bob.lua
@@ -1,7 +1,14 @@
 local bob = {}
 
 function bob.hey(say)
-
+  if say == "" then return 'Fine, be that way.' end
+  normalized = say:gsub('%A', '')
+  is_all_uppercase = string.len(normalized:gsub('%u', '')) == 0
+  last_char = say:sub(-1)
+  if last_char == '?' and is_all_uppercase then return 'Calm down, I know what I\'m doing!' end
+  if last_char == '?' then return 'Sure' end
+  if is_all_uppercase == true then return 'Whoa, chill out!' end
+  return 'Whatever'
 end
 
 return bob
