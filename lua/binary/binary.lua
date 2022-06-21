b6ee0806f37965dd6a6f746a615eed6a9f0630a5
denismaier <denis.maier@ub.unibe.ch>
2022-06-21 08:51:34 +0200

    improve

--- lua/binary/binary.lua
+++ lua/binary/binary.lua
@@ -1,8 +1,8 @@
 local function to_decimal(input)
-  if input:find('%D') then return 0 end
+  if input:match('%D') then return 0 end
   local function recursive_worker(input, factor, acc)
     if #input == 0 then return acc end
-    partial_result = tonumber(input:sub(1,1)) * 2^factor
+    local partial_result = tonumber(input:sub(1,1)) * 2^factor
     return recursive_worker(input:sub(2), factor-1, acc + partial_result)
   end
   return recursive_worker(input, #input - 1, 0)
