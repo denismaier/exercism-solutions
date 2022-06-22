5e61bcfc0f810591c78fc3ed4e270895d53da7a7
denismaier <denis.maier@ub.unibe.ch>
2022-06-22 08:45:47 +0200

    make functions local

--- lua/atbash-cipher/atbash-cipher.lua
+++ lua/atbash-cipher/atbash-cipher.lua
@@ -28,15 +28,7 @@ local mapping = {
   z = "a",
 }
 
-function encode(str)
-  --normalize
-  str = str:lower()
-  str = str:gsub("%W", "")
-  -- transpose and format
-  return string_to_chunks(transpose(str), 5) 
-end
-
-function transpose (str)
+local function transpose (str)
   local letters = {}
   for char in string.gmatch(str, ".") do 
     if char:match('%a') then table.insert(letters, mapping[char]) end
@@ -45,12 +37,20 @@ function transpose (str)
   return table.concat(letters)
 end
 
-function string_to_chunks(str, size)
+local function string_to_chunks(str, size)
   local chunks = {}
   for i=1, #str, size do
     table.insert(chunks, str:sub(i, i+size-1))
   end
   return table.concat(chunks, ' ')
 end
+
+local function encode(str)
+  --normalize
+  str = str:lower():gsub("%W", "")
+  -- transpose and format
+  return string_to_chunks(transpose(str), 5) 
+end
+
 
 return { encode = encode }
