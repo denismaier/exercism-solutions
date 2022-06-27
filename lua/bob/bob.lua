3c1d8b62e08cbf203964a72c46860487c5122310
denismaier <denis.maier@ub.unibe.ch>
2022-06-27 11:24:00 +0200

    small improvements

--- lua/bob/bob.lua
+++ lua/bob/bob.lua
@@ -5,8 +5,7 @@ local function is_question(str)
 end
 
 local function is_shouting(str)
-  normalized = str:gsub('%A', '')
-  is_all_uppercase = string.len(normalized:gsub('%u', '')) == 0
+  is_all_uppercase = string.len(str:gsub('%A', ''):gsub('%u', '')) == 0
   return is_all_uppercase
 end
 
@@ -14,7 +13,7 @@ function bob.hey(say)
   if say == "" then return 'Fine, be that way.' end
   if is_question(say) and is_shouting(say) then return 'Calm down, I know what I\'m doing!' end
   if is_question(say) then return 'Sure' end
-  if is_shouting(say) == true then return 'Whoa, chill out!' end
+  if is_shouting(say) then return 'Whoa, chill out!' end
   return 'Whatever'
 end
 
--- lua/collatz-conjecture/collatz-conjecture.lua
+++ lua/collatz-conjecture/collatz-conjecture.lua
@@ -1,10 +1,10 @@
-function conjecture(n, acc)
+local function conjecture(n, acc)
     assert (n > 0, 'Only positive numbers are allowed')
     local acc = acc or 0
     -- base case
     if n == 1 then return acc end
     -- update accumulator
-    next_acc = acc + 1
+    local next_acc = acc + 1
     -- odd/even handling
     if is_even(n) then 
         return conjecture(n/2, next_acc) 
@@ -13,7 +13,7 @@ function conjecture(n, acc)
     end
 end
 
-function is_even(n)
+local function is_even(n)
     return n % 2 == 0
 end
 
