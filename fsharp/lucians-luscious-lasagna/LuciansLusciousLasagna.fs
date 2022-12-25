7745d395cf7035a70196b919f242ba60b0a6e029
denismaier <maier.de@gmail.com>
2022-12-25 23:40:18 +0100

    new stuff

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,11 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40
+
+let remainingMinutesInOven minInOven = expectedMinutesInOven - minInOven
+
+let preparationTimeInMinutes layers = layers * 2
+
+let elapsedTimeInMinutes layers minInOven = 
+    let prepTime = preparationTimeInMinutes layers
+    minInOven + prepTime
---
+++ fsharp/prime-factors/PrimeFactors.fs
@@ -0,0 +1,3 @@
+module PrimeFactors
+
+let factors number = failwith "You need to implement this function."
